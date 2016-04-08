66277cec6f4d9523fa34bbb0c4df369bf9a24eaf
Yuval Carmel <yuval@singular.net>
2016-04-09 01:14:23 +0300

    adding proxy fix

--- wedweb/__init__.py
+++ wedweb/__init__.py
@@ -1,9 +1,11 @@
 from flask import Flask
 from flask.templating import render_template
+from werkzeug.contrib.fixers import ProxyFix
 
 app = Flask(__name__)
 app.config.from_object('webconfig')
 
+app.wsgi_app = ProxyFix(app.wsgi_app)
 
 @app.errorhandler(404)
 def not_found(error):
