483c068ffec49919e37cc2c235ec608760e9a0cf
Yuval Carmel <yuval@singular.net>
2016-04-16 13:28:47 +0300

    saving timestamp to gdrive

--- wedweb/gdrive/gdrive_writer.py
+++ wedweb/gdrive/gdrive_writer.py
@@ -1,4 +1,5 @@
 import os
+import datetime
 from gdrive_connection import GspreadConnection
 
 class GdriveWriter(object):
@@ -8,6 +9,6 @@ class GdriveWriter(object):
 
     def add_guests_to_gdrive(self, name, phone, number):
         sheet = self.connection.gc.open_by_key(os.environ.get('GDRIVE_FILE_KEY'))
-        sheet.worksheet('Sheet1').append_row([name, phone, number])
+        sheet.worksheet('Sheet1').append_row([name, phone, number, datetime.datetime.now()])
 
 
